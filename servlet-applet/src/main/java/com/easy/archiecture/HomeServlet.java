3da2bde0eca45cf6a69dbf7ec912ea7822f91282
yanghai10 <yanghai10@jd.com>
2024-07-26 16:15:35 +0800

    删除无用的文件

--- servlet-applet/src/main/java/com/easy/archiecture/HomeServlet.java
+++ servlet-applet/src/main/java/com/easy/archiecture/HomeServlet.java
@@ -17,7 +17,6 @@ public class HomeServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-        // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
