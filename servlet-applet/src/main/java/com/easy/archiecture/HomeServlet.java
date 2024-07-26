6a4e469845ab44c99e786109926d4faf6a524ceb
yanghai10 <yanghai10@jd.com>
2024-07-26 15:48:13 +0800

    fix

---
+++ servlet-applet/src/main/java/com/easy/archiecture/HomeServlet.java
@@ -0,0 +1,29 @@
+package com.easy.archiecture;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "helloServlet", value = "/home-servlet")
+public class HomeServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
---
+++ servlet-applet/src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JSP - HOME</title>
+</head>
+<body>
+<h1><%= "我的第一个Web应用!" %>
+</h1>
+<br/>
+<a href="home-servlet">Hello Servlet</a>
+</body>
+</html>
