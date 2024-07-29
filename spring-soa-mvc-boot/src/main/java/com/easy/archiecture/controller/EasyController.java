bed56bf02a72764f7a26cc2889c4c6d52b32cc38
yanghai10 <yanghai10@jd.com>
2024-07-29 21:00:01 +0800

    fix

--- spring-soa-mvc-boot/src/main/java/com/easy/archiecture/controller/EasyController.java
+++ spring-soa-mvc-boot/src/main/java/com/easy/archiecture/controller/EasyController.java
@@ -3,6 +3,7 @@ package com.easy.archiecture.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author yanghai
@@ -11,9 +12,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @date 2024/7/28 16:44
  */
 @Controller
+@ResponseBody
+@RequestMapping("/home")
 public class EasyController {
 
-    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    @RequestMapping(value = "/index")
     public String home() {
         return "index";
     }
