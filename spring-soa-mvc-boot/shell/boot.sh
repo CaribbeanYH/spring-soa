f703c4531faf0b1d0ef6fd4b3776d7e06b8c67cf
yanghai10 <yanghai10@jd.com>
2024-07-30 21:52:26 +0800

    fix

--- spring-soa-mvc-boot/shell/boot.sh
+++ spring-soa-mvc-boot/shell/boot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "mvc_boot app start running"
-java -jar -Xms5324m  -Xmx5324m  -XX:MaxMetaspaceSize=256m -XX:MetaspaceSize=256m  -XX:MaxDirectMemorySize=983m  -XX:ConcGCThreads=0  -XX:ParallelGCThreads=4  -XX:CICompilerCount=2 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/Users/yanghai10/Documents/athena/spring-soa/spring-soa-mvc-boot/log /Users/yanghai10/Documents/athena/spring-soa/spring-soa-mvc-boot/target/spring-mvc-boot.jar
+java -jar -Xms5324m  -Xmx5324m  -XX:MaxMetaspaceSize=256m -XX:MetaspaceSize=256m  -XX:MaxDirectMemorySize=983m  -XX:ConcGCThreads=0  -XX:ParallelGCThreads=4  -XX:CICompilerCount=2 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/Users/yanghai10/Documents/athena/spring-soa/spring-soa-mvc-boot/log /Users/yanghai10/Documents/athena/spring-soa/spring-soa-mvc-boot/target/spring-soa-mvc-boot-1.0-SNAPSHOT.jar
