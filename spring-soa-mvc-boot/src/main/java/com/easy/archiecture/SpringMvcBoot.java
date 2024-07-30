0104cc12369fa54e37ca1d106fb5b97d5765bec5
yanghai10 <yanghai10@jd.com>
2024-07-30 18:00:14 +0800

    fix

--- spring-soa-mvc-boot/src/main/java/com/easy/archiecture/SpringMvcBoot.java
+++ spring-soa-mvc-boot/src/main/java/com/easy/archiecture/SpringMvcBoot.java
@@ -28,8 +28,18 @@ public class SpringMvcBoot {
         resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes", additionWebInfClasses.getAbsolutePath(), "/"));
         context.setResources(resources);
         tomcat.start();
+        // 创建一个URI实例
+        java.net.URI uri = java.net.URI.create("http://localhost:8080");
+        // 获取当前系统桌面扩展
+        java.awt.Desktop desktop = java.awt.Desktop.getDesktop();
+        // 判断系统桌面是否支持要执行的功能
+        if (desktop.isSupported(java.awt.Desktop.Action.BROWSE)) {
+            // 获取系统默认浏览器打开链接
+            desktop.browse(uri);
+        }
         // 服务阻塞等待
         tomcat.getServer().await();
+
     }
 
     /**
